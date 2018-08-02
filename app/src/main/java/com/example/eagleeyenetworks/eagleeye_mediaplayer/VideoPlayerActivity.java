6dbfdeabe03c74220b8db1cf813a0a24815a3380
Greg Slomin <gslomin@eagleeyenetworks.com>
2018-08-02 12:16:54 -0500

    Finished

--- app/src/main/java/com/example/eagleeyenetworks/eagleeye_mediaplayer/VideoPlayerActivity.java
+++ app/src/main/java/com/example/eagleeyenetworks/eagleeye_mediaplayer/VideoPlayerActivity.java
@@ -24,19 +24,30 @@ import com.google.android.exoplayer2.upstream.DefaultHttpDataSourceFactory;
 import java.util.Random;
 
 public class VideoPlayerActivity extends AppCompatActivity {
-    PlayerView playerView;
-    ExoPlayer player;
+    protected PlayerView playerView;
+    protected ExoPlayer player;
+    protected String cameraID;
+    protected String authKey;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_video_player);
 
         Intent intent = getIntent();
-        final String esn = intent.getStringExtra("esn");
-        final String authToken = intent.getStringExtra("auth_token");
-
+        cameraID = intent.getStringExtra("esn");
+        authKey = intent.getStringExtra("auth_token");
         playerView = findViewById(R.id.player_view);
-        LoadControl loadControl = new DefaultLoadControl(new DefaultAllocator(true, C.DEFAULT_BUFFER_SEGMENT_SIZE),
+
+        String url = String.format("https://login.eagleeyenetworks.com/asset/play/video.flv?c=%s;t=stream_%d;e=+300000;A=%s",
+                cameraID,
+                new Random().nextInt(),
+                authKey);
+
+        Uri uri = Uri.parse(url);
+
+        LoadControl loadControl = new DefaultLoadControl(
+                new DefaultAllocator(true, C.DEFAULT_BUFFER_SEGMENT_SIZE),
                 25,
                 25000,
                 25,
@@ -44,14 +55,11 @@ public class VideoPlayerActivity extends AppCompatActivity {
                 10 * 1024 * 1024,
                 true);
 
+        MediaSource source = new ExtractorMediaSource.Factory(new DefaultHttpDataSourceFactory("Eagle Eye Networks - Android")).createMediaSource(uri);
+
         player = ExoPlayerFactory.newSimpleInstance(new DefaultRenderersFactory(this), new DefaultTrackSelector(), loadControl);
         player.setPlayWhenReady(true);
         playerView.setPlayer(player);
-
-        String url = String.format("https://login.eagleeyenetworks.com/asset/play/video.flv?c=%s;t=stream_%d;e=+300000;A=%s", esn, new Random().nextInt(), authToken);
-        Log.e("URL", url);
-        Uri uri = Uri.parse(url);
-        MediaSource source = new ExtractorMediaSource.Factory(new DefaultHttpDataSourceFactory("Eagle Eye Networks - Android")).createMediaSource(uri);
         player.prepare(source ,false, false);
 
     }
